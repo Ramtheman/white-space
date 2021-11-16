2834c6c26e789146b18011eede7d6e3af241fb8b
Rami Assaf <ramiw.assaf@gmail.com>
2021-11-16 11:42:28 +0000

    implementing seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+testers = {
+  Rami: { first_name: "Rami", last_name: "Assaf", email: "ramiw.assaf@gmail.com", password: 'password' },
+  Simon: { first_name: "Simon", last_name: "Foster", email: "simonfos74@gmail.com", password: 'password' },
+  Will: { first_name: "Will", last_name: "Holmes", email: "willholmes60@gmail.com", password: 'password' },
+  Mark: { first_name: "Mark", last_name: "de Courcy Ling", email: "m.decourcyling@gmail.com", password: 'password' }
+}
+
+puts testers
