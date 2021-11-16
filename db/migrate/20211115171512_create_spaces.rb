c62aec5f73bbc69736d2fe5218452791d5fe6960
Rami Assaf <ramiw.assaf@gmail.com>
2021-11-16 13:06:29 +0000

    doing changes to spaces migration so i dont need an update migration afterwards

--- db/migrate/20211115171512_create_spaces.rb
+++ db/migrate/20211115171512_create_spaces.rb
@@ -2,7 +2,7 @@ class CreateSpaces < ActiveRecord::Migration[6.0]
   def change
     create_table :spaces do |t|
       t.string :title
-      t.string :type
+      t.string :space_type
       t.integer :price
       t.references :user, null: false, foreign_key: true
 
