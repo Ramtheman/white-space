c73bc93c32af94ad596046c9474e1646b16f6894
Will Holmes <willholmes60@gmail.com>
2021-11-17 13:53:02 +0000

    spaces styling plus setting up geocoding and spaces map

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,5 +1,8 @@
 class Space < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  validates :title, :space_type, :price, presence: true
+  validates :title, :space_type, :price, :location, presence: true
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
