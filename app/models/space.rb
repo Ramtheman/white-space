6758a9721667f1da6b583718aac3ca31e8258868
Mark de Courcy Ling <m.decourcyling@gmail.com>
2021-11-17 15:32:40 +0000

    Added space type validation

--- app/models/space.rb
+++ app/models/space.rb
@@ -3,6 +3,9 @@ class Space < ApplicationRecord
   has_many :bookings
   validates :title, :space_type, :price, :location, presence: true
 
+  validates :space_type, inclusion: { in: ['Face paint', 'Bathroom stall', 'Tattoo', 'Manhole', 'Legal name', 'Oyster card'],
+    message: "%{value} is not a valid space type" }
+
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
 end
